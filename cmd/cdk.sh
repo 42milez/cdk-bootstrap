285693c96304076e967162d90c55c90b5a7f93aa
Akihiro TAKASE <42milez@gmail.com>
2020-09-28 20:32:15 +0900

    support --help option

--- cmd/cdk.sh
+++ cmd/cdk.sh
@@ -20,6 +20,11 @@ positional=("$1") && shift # store sub-command
 
 while [ $# -gt 0 ]; do
 {
+  if [ "$1" = '--help' ]; then
+    read_yaml "${PROJECT_ROOT}/cmd/option.yml" "cdk.${positional[0]}.--help.val"
+    exit 0
+  fi
+
   opt=$(read_yaml "${PROJECT_ROOT}/cmd/option.yml" "cdk.${positional[0]}.$1.val")
 
   # skip positional argument
